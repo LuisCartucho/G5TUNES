84a2be3c6caddeff1f6ffe5d9ac12bfdf3f974a3
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2024-11-26 09:51:39 +0100

    added main view
    added main view

--- src/main/java/easv/g5tunes/HelloApplication.java
+++ src/main/java/easv/g5tunes/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws WorkoutExceptions {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MyTunesMainView.fxml"));
         Scene scene = null;
         try {
             scene = new Scene(fxmlLoader.load());
