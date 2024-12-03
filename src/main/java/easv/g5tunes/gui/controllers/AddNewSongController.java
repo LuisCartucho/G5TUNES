7ce7562abe5256ad52440e9baa4ac2c1c2b71fe3
Emre <aliemreuzunoglu@gmail.com>
2024-12-03 10:37:38 +0100

    Renamed the fxml files and their controllers

--- src/main/java/easv/g5tunes/gui/controllers/AddNewSongController.java
+++ src/main/java/easv/g5tunes/gui/controllers/AddNewSongController.java
@@ -3,7 +3,7 @@ package easv.g5tunes.gui.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class NewEditSongControllerV2 {
+public class AddNewSongController {
 
     @FXML
     private Button btnSave;
--- src/main/java/easv/g5tunes/gui/controllers/EditSongController.java
+++ src/main/java/easv/g5tunes/gui/controllers/EditSongController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-public class NewEditSongController {
+public class EditSongController {
     @FXML
     private TextField txtFieldTitle;
     @FXML
