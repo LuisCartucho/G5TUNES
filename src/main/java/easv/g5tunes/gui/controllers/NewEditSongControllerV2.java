b53fee3d1b9f5727437052b33eaf697de90daecb
Emre <aliemreuzunoglu@gmail.com>
2024-12-03 10:00:25 +0100

    Created new New/Edit Song fxml and controller for GUI

---
+++ src/main/java/easv/g5tunes/gui/controllers/NewEditSongControllerV2.java
@@ -0,0 +1,10 @@
+package easv.g5tunes.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class NewEditSongControllerV2 {
+
+    @FXML
+    private Button btnSave;
+}
